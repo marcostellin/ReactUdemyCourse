a9e0bbdfb20c00d547b29066b65ecd8c2fdaae14
Marco Stellin <stellin.marco@mailbox.org>
2020-03-24 10:09:30 +0100

    Add logic to handle adding ingredients

--- burger-app/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-app/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classes from './BurgerIngredient.css';
-import PropTypes, { string } from 'prop-types';
+import PropTypes from 'prop-types';
 
 class BurgerIngredient extends Component {
     render () {
@@ -10,7 +10,6 @@ class BurgerIngredient extends Component {
         switch (this.props.type) {
             case ('bread-bottom'):
                 return <div className={classes.BreadBottom}></div>;
-                break;
             case ('bread-top'):
                 return (
                     <div className={classes.BreadTop}>
@@ -18,19 +17,14 @@ class BurgerIngredient extends Component {
                         <div className={classes.Seeds2}></div>
                     </div>
                 );
-                break;
             case ('meat'):
                 return <div className={classes.Meat}></div>;
-                break;
             case ('cheese'):
                 return <div className={classes.Cheese}></div>;
-                break;
             case ('salad'):
                 return <div className={classes.Salad}></div>;
-                break;
             case ('bacon'):
                 return <div className={classes.Bacon}></div>;
-                break;
             default:
                 ingredient = null;
 
--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.4,
+    cheese: 0.5,
+    meat: 1.3,
+    bacon: 0.7
+}
+
 class BurgerBuilder extends Component {
 
     state = {
@@ -11,14 +18,28 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const ingredients = {...this.state.ingredients};
+        ingredients[type] = ingredients[type] + 1;
+        this.setState({ingredients: ingredients});
+        this.setState({totalPrice: this.state.totalPrice + INGREDIENT_PRICES[type]});
+    }
+
+    removeIngredientHandler = (type) => {
+
     }
 
     render () {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <BuildControls />
+                <BuildControls 
+                    ingredientAdded={this.addIngredientHandler}
+                    />
             </Aux>
         );
     }
