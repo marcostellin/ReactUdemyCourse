b62c4dc3c2f88c19c04b987324166bbad6f9c377
Marco Stellin <stellin.marco@mailbox.org>
2020-03-24 11:15:03 +0100

    Add Order Now button and logic

--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,7 +19,19 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
+    }
+
+    updatePurchaseState (ingredients) {
+        const sum = Object.keys(ingredients)
+                    .map((igKey) => {
+                        return ingredients[igKey];
+                    })
+                    .reduce( (sum, el) => {
+                        return sum + el;
+                    }, 0);
+        this.setState({purchasable: sum > 0});
     }
 
     addIngredientHandler = (type) => {
@@ -27,6 +39,7 @@ class BurgerBuilder extends Component {
         ingredients[type] = ingredients[type] + 1;
         this.setState({ingredients: ingredients});
         this.setState({totalPrice: this.state.totalPrice + INGREDIENT_PRICES[type]});
+        this.updatePurchaseState(ingredients);
     }
 
     removeIngredientHandler = (type) => {
@@ -37,6 +50,8 @@ class BurgerBuilder extends Component {
             this.setState({ingredients: ingredients});
             this.setState({totalPrice: this.state.totalPrice - INGREDIENT_PRICES[type]});
         } 
+
+        this.updatePurchaseState(ingredients);
     }
 
     render () {
@@ -54,6 +69,7 @@ class BurgerBuilder extends Component {
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
                     price={this.state.totalPrice}
+                    purchasable={this.state.purchasable}
                     />
             </Aux>
         );
