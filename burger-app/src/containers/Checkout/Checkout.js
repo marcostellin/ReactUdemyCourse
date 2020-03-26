62a426975e4c3a32d42d89e39e5b7f2d6fd24c71
Marco Stellin <stellin.marco@mailbox.org>
2020-03-26 16:30:10 +0100

    Add Contact Data form when checking out

--- burger-app/src/containers/Checkout/Checkout.js
+++ burger-app/src/containers/Checkout/Checkout.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import {Route} from 'react-router-dom';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
 
     state = {
         ingredients: {
-            cheese: 1,
             salad: 1,
             meat: 1,
-            bacon: 1
+            bacon: 1,
+            cheese: 1
         }
     }
 
@@ -36,6 +38,7 @@ class Checkout extends Component {
                 ingredients={this.state.ingredients}
                 checkoutCancelled={this.checkoutCancelledHandler}
                 checkoutContinued={this.checkoutContinuedHandler}/>
+            <Route path={this.props.match.url + '/contact-data'} component={ContactData}/>
         </div>);
     }
 }
