b1da32f55376737f6d5e92bfd0990d5fdc17187d
Marco Stellin <stellin.marco@mailbox.org>
2020-03-27 12:38:39 +0100

    Add select in custom Input component

--- burger-app/src/components/UI/Input/Input.js
+++ burger-app/src/components/UI/Input/Input.js
@@ -13,6 +13,14 @@ const input = (props) => {
         case ('textarea'):
             inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value}/>
             break;
+        case ('select'):
+            inputElement = (
+                            <select className={classes.InputElement} value={props.value} >
+                                {props.elementConfig.options.map(option => {
+                                    return <option key={option.value} value={option.value}>{option.displayValue}</option>
+                                })}
+                            </select>);
+            break;
         default:
             inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value}/>
     }
--- burger-app/src/containers/Checkout/ContactData/ContactData.js
+++ burger-app/src/containers/Checkout/ContactData/ContactData.js
@@ -97,7 +97,7 @@ class ContactData extends Component {
                     return (
                         <Input 
                             key={formElement.id}
-                            elementType={formElement.config.elementType}
+                            inputtype={formElement.config.elementType}
                             elementConfig={formElement.config.elementConfig}
                             value={formElement.config.value}/>
                     );
