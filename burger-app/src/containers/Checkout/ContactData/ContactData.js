6b93911297d0a71400f0fd2a2820d7e22696210a
Marco Stellin <stellin.marco@mailbox.org>
2020-03-27 12:56:47 +0100

    Sudmit contact data when placing an order

--- burger-app/src/containers/Checkout/ContactData/ContactData.js
+++ burger-app/src/containers/Checkout/ContactData/ContactData.js
@@ -65,10 +65,16 @@ class ContactData extends Component {
     orderHandler = (event) => {
         event.preventDefault();
         this.setState({loading: true});
+        const formData = {};
+
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
 
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.price
+            price: this.props.price,
+            orderData: formData
         }
 
         axios.post('/orders.json', order)
@@ -101,7 +107,7 @@ class ContactData extends Component {
         }
 
         let form = (
-            <form>
+            <form onSubmit={this.orderHandler}>
                 {formElementArray.map( formElement => {
                     return (
                         <Input 
@@ -112,7 +118,7 @@ class ContactData extends Component {
                             changed={(event) => this.inputChangedHandler(event, formElement.id)} />
                     );
                 })}
-                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+                <Button btnType="Success">ORDER</Button>
             </form>
         );
 
